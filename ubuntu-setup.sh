bb7ab8046096262e805c1f5cb7b93a5c07d4de98
Harrison Saliba <harrysaliba4139@gmail.com>
2022-03-06 02:20:08 -0800

    Switched to homebuilt data tracking.

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -7,3 +7,6 @@ sudo apt update
 # install docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
+sudo apt install docker-compose
+sudo systemctl enable docker
+sudo docker-compose up -d
